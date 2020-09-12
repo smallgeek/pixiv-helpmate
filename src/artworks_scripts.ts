3120bdf560c7cf4c0a9010f2647893260812e8c5
smallgeek <smallgeek@outlook.jp>
2020-09-12 21:13:48 +0900

    画像取得の足掛かりを作成

--- src/artworks_scripts.ts
+++ src/artworks_scripts.ts
@@ -29,36 +29,47 @@ function addModeButton() {
  * グレースケールボタンが押されたときの処理
  * @param evt 
  */
-function onGrayScaleButtonClick(evt: Event) {
-    const source = <HTMLImageElement>document.querySelector('figure.sc-1yvhotl-3.jFNHIP img');
-
+async function onGrayScaleButtonClick(evt: Event) {
     const image = document.createElement('img');
     image.crossOrigin = "Anonymous";
-    image.onload = () => {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-        if (!ctx) {
-            return;
-        }
-        ctx.drawImage(image, 0, 0, image.width, image.height);
-        
-        // ここでCORS
-        let src = ctx.getImageData(0, 0, image.width, image.height);
-        let dst = ctx.createImageData(image.width, image.height);
 
-        for (let i = 0; i < src.data.length; i += 4) {
-            let y = 0.2126 * src.data[i] + 0.7152 * src.data[i + 1] + 0.0722 * src.data[i + 2];
-            y = parseInt(y.toString(), 10);
-            dst.data[i] = y;
-            dst.data[i + 1] = y;
-            dst.data[i + 2] = y;
-            dst.data[i + 3] = src.data[i + 3];
-        }
-        ctx.putImageData(dst, 0, 0);
-        
-        document.querySelector('figure')?.appendChild(canvas);
-    };
-    image.src = source.src;
+    try {
+        const source = <HTMLImageElement>document.querySelector('figure.sc-1yvhotl-3.jFNHIP img');
+        const a = <HTMLAnchorElement>source.parentElement;
+        const response = await fetch(a.href, { mode: 'no-cors' });
+
+        // blob のサイズが 0 なので何かが間違っている。
+        const blob = await response.blob();
+        const url = (window.URL || window.webkitURL).createObjectURL(blob);
+        image.src = url;
+    } catch (error) {
+        console.log(error);    
+    }
+
+    /*
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    if (!ctx) {
+        return;
+    }
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+    
+    // ここでCORS
+    let src = ctx.getImageData(0, 0, image.width, image.height);
+    let dst = ctx.createImageData(image.width, image.height);
+
+    for (let i = 0; i < src.data.length; i += 4) {
+        let y = 0.2126 * src.data[i] + 0.7152 * src.data[i + 1] + 0.0722 * src.data[i + 2];
+        y = parseInt(y.toString(), 10);
+        dst.data[i] = y;
+        dst.data[i + 1] = y;
+        dst.data[i + 2] = y;
+        dst.data[i + 3] = src.data[i + 3];
+    }
+    ctx.putImageData(dst, 0, 0);
+    */
+    
+    document.querySelector('figure.sc-1yvhotl-3.jFNHIP')?.appendChild(image);
 }
 
 observer.observe(document.body, {childList: true, subtree: true});
